6488a32be27c5843304c2cda3569fbbefea79c1f
Tristan Pacba <tpacba@live.com>
2020-10-07 00:39:25 -0700

    fixed callback parameters

--- server.js
+++ server.js
@@ -1,59 +1,81 @@
-require('dotenv').config()
-let express = require('express')
-let request = require('request')
-let querystring = require('querystring')
+const express = require("express");
 
-let app = express()
-let PORT = process.env.PORT || 8888
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 8888;
 
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-let SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-let SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
-let redirect_uri =
-    process.env.REDIRECT_URI ||
-    'http://localhost:8888/api/callback'
+// Add routes, both API and view
+app.use(routes);
 
-app.get('/login', function (req, res) {
-    console.log("Going to redirect /login");
-
-    res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-            response_type: 'code',
-            client_id: SPOTIFY_CLIENT_ID,
-            scope: 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-private user-read-private',
-            redirect_uri: redirect_uri
-        }))
-})
-
-app.get('/callback', function (req, res) {
-    console.log("Going to redirect /callback");
-
-    let code = req.query.code || null
-    let authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-            code: code,
-            redirect_uri: redirect_uri,
-            grant_type: 'authorization_code'
-        },
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer(
-                SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
-            ).toString('base64'))
-        },
-        json: true
-    }
-    request.post(authOptions, function (error, response, body) {
-        var access_token = body.access_token
-        let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-        res.redirect(uri + '/#' + querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-        }))
-    })
-})
-
+// Start the API server
 app.listen(PORT, () =>
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
 );
+
+// require('dotenv').config()
+// let express = require('express')
+// let request = require('request')
+// let querystring = require('querystring')
+
+// let app = express()
+// let PORT = process.env.PORT || 8888
+
+// let SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+// let SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+
+// let redirect_uri =
+//     process.env.REDIRECT_URI ||
+//     'http://localhost:8888/api/callback'
+
+// app.get('/login', function (req, res) {
+//     console.log("Going to redirect /login");
+
+//     res.redirect('https://accounts.spotify.com/authorize?' +
+//         querystring.stringify({
+//             response_type: 'code',
+//             client_id: SPOTIFY_CLIENT_ID,
+//             scope: 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-private user-read-private',
+//             redirect_uri: redirect_uri
+//         }))
+// })
+
+// app.get('/api/callback', function (req, res) {
+//     console.log("Going to redirect /callback");
+
+//     let code = req.query.code || null
+//     let authOptions = {
+//         url: 'https://accounts.spotify.com/api/token',
+//         form: {
+//             code: code,
+//             redirect_uri: redirect_uri,
+//             grant_type: 'authorization_code'
+//         },
+//         headers: {
+//             'Authorization': 'Basic ' + (new Buffer(
+//                 SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+//             ).toString('base64'))
+//         },
+//         json: true
+//     }
+//     request.post(authOptions, function (error, response, body) {
+//         var access_token = body.access_token
+//         let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+//         res.redirect(uri + '/#' + querystring.stringify({
+//             access_token: access_token,
+//             refresh_token: refresh_token
+//         }))
+//     })
+// })
+
+// app.listen(PORT, () =>
+//     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+// );
